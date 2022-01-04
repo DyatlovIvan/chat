2389a836fa1764147cd144b821a640c5740181f1
DyatlovIvan <diatlovivan92@mail.ru>
2022-01-04 23:03:26 +0400

    initial commit. use webSocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type UsersType = {
+    userId: number
+    userName: string
+    message: string
+    photo:string
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('')
+    const [users, setUsers] = useState<Array<UsersType>>([])
+    const [webSocked, setWebSocked] = useState<null | WebSocket>(null)
+
+    useEffect(() => {
+        const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+        ws.onmessage = (messageEvent) => {
+            setUsers([...users, ...JSON.parse(messageEvent.data)])
+        }
+        setWebSocked(ws);
+
+    }, [])
+    const onMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.currentTarget.value)
+    }
+    const sendMessage = () => {
+        if (webSocked) {
+            webSocked.send(message)
+        }
+
+    }
+    return (
+        <div className="App">
+
+            <div className={'chat'}>
+                <div className={'messages'}>
+                    {users.map((m, index) =>
+                        <div className={'message'} key={index}>
+                            <img src={m.photo} alt="avatar"/>
+                            <b>{m.userName}</b>
+                            <span>{m.message}</span>
+                        </div>
+                    )}
+
+                </div>
+
+                <div className={'footer'}>
+                    <textarea value={message} onChange={onMessageChange}></textarea>
+                    <button onClick={sendMessage}>Send</button>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
